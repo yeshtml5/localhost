b1c6ec0293a076fa104939a88c0f8a27ac39d6f8
wanhwi.son <wanhwi.son@blockchainos.org>
2018-02-09 19:13:27 +0900

    angularjs v 0.1

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
 var app = angular.module('localApp', ['ngRoute']);
-
+/*==================================================
+   Router
+==================================================*/
 app.config(function ($routeProvider) {
     $routeProvider.when('/', {
-        templateUrl: '/pages/main.html',
+        templateUrl: '/pages/main/index.html',
         controller: 'mainCtrl'
     }).when('/button', {
         templateUrl: '/pages/style/button.html',
         controller: 'buttonCtrl'
+    }).when('/error', {
+        templateUrl: '/pages/etc/error.html',
+        controller: 'buttonCtrl'
     }).otherwise({
-        redirectTo: '/'
+        redirectTo: '/error'
     });
 });
+/*==================================================
+   Templete
+==================================================*/
+app.directive('ngHeader', function () {
+    return {
+        templateUrl: '/template/header.html'
+    }
+});
+app.directive('ngFooter', function () {
+    return {
+        templateUrl: '/template/footer.html'
+    }
+});
+/*==================================================
+   Controller
+==================================================*/
 app.controller('localCtrl', function ($scope) {
+    $scope.title = "이것은 페이지 타이틀이다.";
     $scope.menu1 = "dkdd";
 });
 
--- pages/style/buttonCtrl.js
+++ pages/style/buttonCtrl.js
@@ -1,8 +1,8 @@
-app.controller('buttonCtrl', function ($scope, $routeParams) {
+angular.module('localApp').controller('buttonCtrl', function ($scope, $routeParams) {
     $scope.clickFunc = function () {
         //alert('22ddd2');
-        alert( $('button').length );
-        console.log( $ );
+        alert($('button').length);
+        document.title = 'test';
         //$('button').removeClass('btn-blue');
     };
 });
