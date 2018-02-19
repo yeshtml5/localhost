e431c919635f4bde20cbc83a47320450346324ee
wanhwi.son <wanhwi.son@blockchainos.org>
2018-02-19 15:08:52 +0900

    add commit

--- pages/ajax/ajax.js
+++ pages/ajax/ajax.js
@@ -2,11 +2,10 @@ angular.module('localApp').factory('ajaxStorage', function ($http) {
     var storage = {
         load: function (url) {
             var info = $http.get(url).then(function (res) {
-                console.log(res.data);
+           //     console.log(res.data);
                 return res.data;
             });
             return info;
-
         },
         get: function () {
             return storage.data;
--- pages/ajax/listCtrl.js
+++ pages/ajax/listCtrl.js
@@ -2,29 +2,22 @@
 * https://stackoverflow.com/questions/22408790/angularjs-passing-data-between-pages
 * */
 angular.module('localApp').controller('listCtrl', function ($scope, ajaxStorage, $http) {
-    var obj = ajaxStorage.load('/src/json/list.json');
-
-    console.log(obj.data);
-    var ajax = "/src/json/list.json";
-
 
     $http.get(ajax).then(function (res) {
         //data
         $scope.lists = res.data;
-
     });
 
     $scope.clickFunc = function (list, index) {
         console.log('------');
         console.log(index);
         document.title = 'test';
-
         //$('button').removeClass('btn-blue');
     };
     (function ($) {
         $(document).ready(function () {
-            return false;
-            $('li img').bind('click', function (event) {
+            //return false;
+            $('a .btn-blue').bind('click', function (event) {
                 event.preventDefault();
                 var _os = $.getOs();
                 console.log(_os);
